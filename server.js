e0b87c4a44d1dcec3542ce4d6445d4d00345cb88
MohmadAyman <no.future2e1@gmail.com>
2016-04-22 18:10:19 +0200

    serverjs port edited

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ var userRouter = require('./src/routes/userRouter.js')();
 var clientRouter = require('./src/routes/clientRouter.js')(io);
 var adminRouter = require('./src/routes/adminRouter.js');
 
-var port = process.env.OPENSHIFT_NODEJS_PORT || '8080';
+var port = process.env.OPENSHIFT_NODEJS_PORT || '8181';
 var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
 
 app.use(express.static('public'));
